78fb61514ac9b1e67e91bb7592f5a2a1d65da0c4
Jordan Atwood <jordan.atwood423@gmail.com>
2018-04-12 23:13:12 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Zulrah Guide
+
+Based on [http://zulrahguide.com](https://web.archive.org/web/20180224034652/http://zulrahguide.com/),
+re-created for continued use and future updates.
+
+Credit to JonTarg for the original ZulrahGuide.com website
---
+++ script.js
@@ -0,0 +1,87 @@
+const CLICKABLE_IMAGES_SELECTOR = '.phase-options img',
+	GUIDE_ELEMENTS_SELECTOR = '.guide > div',
+	GEAR_MENU_ITEMS_SELECTOR = '.gear-menu > li',
+	GEAR_ELEMENTS_SELECTOR = '.gear-content-background > div',
+	HIDDEN_ELEMENT_STYLE = 'display: none;',
+	VISIBLE_ELEMENT_STYLE = 'display: block;';
+
+// Add click handlers to intro hider, clickable images, reset button
+document.addEventListener('DOMContentLoaded', function() {
+	// Rotation Guide click handlers
+	const introHider = document.getElementsByClassName('hide-intro'),
+		resetButton = document.getElementsByClassName('reset'),
+		clickableImages = document.querySelectorAll(CLICKABLE_IMAGES_SELECTOR),
+		gearGuideMenuItems = document.querySelectorAll(GEAR_MENU_ITEMS_SELECTOR);
+	for (const node of introHider) {
+		node.addEventListener('click', hideLinkedClasses, false);
+	}
+	for (const node of resetButton) {
+		node.addEventListener('click', showLinkedClassNodes, false);
+	}
+	for (const img of clickableImages) {
+		img.addEventListener('click', showLinkedClassNodes, false);
+	}
+
+	// Gear Guide click handlers
+	for (const node of gearGuideMenuItems) {
+		node.addEventListener('click', showLinkedClassNodes, false);
+	}
+}, false);
+
+function hideLinkedClasses() {
+	const classesToHide = this.dataset['closes'].split(' ') || [];
+	for (const className of classesToHide) {
+		hideElementsOfClassName(className);
+	}
+}
+
+function showLinkedClassNodes() {
+	const classesToShow = this.dataset['opens'].split(' ');
+	hideAllGuideNodes();
+	hideAllGearNodes();
+	showGuideElementsOfClassNames(classesToShow);
+}
+
+function showGuideElementsOfClassNames(classNames) {
+	for (const className of classNames) {
+		showGuideElementsOfClassName(className);
+	}
+}
+
+function showGuideElementsOfClassName(className) {
+	for (const node of document.getElementsByClassName(className)) {
+		showNode(node);
+	}
+}
+
+function hideAllGuideNodes() {
+	hideNodesOfSelector(GUIDE_ELEMENTS_SELECTOR);
+}
+
+function hideAllGearNodes() {
+	hideNodesOfSelector(GEAR_ELEMENTS_SELECTOR);
+}
+
+function hideElementsOfClassName(className) {
+	for (const node of document.getElementsByClassName(className)) {
+		hideNode(node);
+	}
+}
+
+function hideNodesOfSelector(selector) {
+	for (const node of document.querySelectorAll(selector)) {
+		hideNode(node);
+	}
+}
+
+function hideNode(node) {
+	if (node.getAttribute('style') !== HIDDEN_ELEMENT_STYLE) {
+		node.setAttribute('style', HIDDEN_ELEMENT_STYLE);
+	}
+}
+
+function showNode(node) {
+	if (node.getAttribute('style') !== VISIBLE_ELEMENT_STYLE) {
+		node.setAttribute('style', VISIBLE_ELEMENT_STYLE);
+	}
+}
